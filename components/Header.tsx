89fd17442e0f3f51e32202c9326a5920bc873e41
raulnicolasagusto <raulnicolasagusto@gmail.com>
2025-02-17 21:06:47 -0300

    seguimos con el header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 
 import DarkModeToggle from "./DarkModeToggle";
+import Link from "next/link";
+import { BookMarkedIcon, BookOpen } from "lucide-react";
 
 const Header = () => {
   return (
@@ -8,13 +10,30 @@ const Header = () => {
             <div className="flex justify-between items-center gap-4">
                 {/* left */}
                 <div className="flex items-center gap-4">
-
+                    <Link 
+                    href="/"
+                    prefetch={false}
+                    className="flex items-center space-x-2 hover:opacity-90 transition-opacity"
+                    >
+                        <BookOpen className="h-6 w-6 text-primary" />
+                        <span className="text-xl font-bold bg-gradient-to-r from-primary/90 to-primary bg-clip-text text-transparent">Cursos</span>
+                    </Link>
+                {/* <SearchInput /> */}
                 </div>
                 {/* Right */}
                 <div className="flex items-center space-x-2 md:space-x-4">
-
+                    <nav>
+                        <Link
+                         href="/"
+                         prefetch={false}
+                         className="flex items-center space-x-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors md:border md_border-border md:rounded-md md_px-4 md:py-2"
+                        >
+                           <BookMarkedIcon className="h-4 w-4" />
+                           <span className="hidden md:block">Mis Cursos</span>
+                        </Link>
+                    </nav>
+                    {/* <DarkModeToggle /> */}
                 </div>
-                <DarkModeToggle />
 
             </div>
             
