c2d54eb162c815f0d49ee851f068e28b274be9ec
raulnicolasagusto <raulnicolasagusto@gmail.com>
2025-03-02 22:23:44 -0300

    mostrando imagen del curso

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,6 +1,9 @@
 import { GetCoursesQueryResult } from '@/sanity.types';
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 import  Link  from 'next/link';
 import React from 'react'
+import { Loader } from "@/components/ui/Loader";
 
 interface CourseCardProps {
   course: GetCoursesQueryResult[number];
@@ -15,7 +18,43 @@ function CourseCard({course, progress, href}: CourseCardProps) {
       prefetch={false}
       className="group hover:no-underline flex"
     >
-        <div className="bg-card rounded-xl overflow-hidden shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px] border border-border flex flex-col flex-1">{course.title}</div>
+        <div className="bg-card rounded-xl overflow-hidden shadow-lg transition-all duration-300 ease-in-out 
+        hover:shadow-xl hover:translate-y-[-4px] border border-border flex flex-col flex-1">
+            <div className='relative h-52 w-full overflow-hidden'>
+            {course.image ? (
+                <Image
+                src={urlFor(course.image).url() || ""}
+                alt={course.title || "Course Image"}
+                fill
+                className="object-cover transition-transform duration-300 group-hover:scale-110"
+                />
+                 ) : (
+                <div className="h-full w-full flex items-center justify-center bg-muted">
+                <Loader size="lg" />
+                </div>
+                )}
+                {/*  titulo y precio del curso */}
+                <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent opacity-60 group-hover:opacity-80 transition-opacity duration-300" />
+                <div className="absolute bottom-4 left-4 right-4 flex items-center justify-between">
+                    <span className="text-sm font-medium px-3 py-1 bg-black/50 text-white rounded-full backdrop-blur-sm">
+                    {course.category?.name || "Uncategorized"}
+                    </span>
+                    {"price" in course && typeof course.price === "number" && (
+                    <span className="text-white font-bold px-3 py-1 bg-black/50 dark:bg-white/20 rounded-full backdrop-blur-sm">
+                        {course.price === 0
+                        ? "Free"
+                        : `$${course.price.toLocaleString("en-US", {
+                            minimumFractionDigits: 2,
+                            })}`}
+                    </span>
+                    )}
+                </div>
+        {/*  fin titulo y precio del curso */}
+            
+            
+            
+            </div>
+         </div>
     </Link>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['cdn.sanity.io'], // Agrega el dominio de Sanity
+  },
 };
 
 export default nextConfig;
